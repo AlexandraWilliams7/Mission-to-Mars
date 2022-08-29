80e9efcd178041ab6cf946c7ad0a807b451742be
AlexandraWilliams7 <a.lain@Alexandra-Williams.com>
2022-08-29 17:10:01 -0500

    changing file to scraping.py and updating code.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # import dependencies
 from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo 
+from flask_pymongo import PyMongo
 import scraping
 
 # make app for flask
